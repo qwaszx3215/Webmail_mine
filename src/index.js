5f8f2713313fa4ac525833117cb10b3a04fdb259
qwaszx3215 <84395173+qwaszx3215@users.noreply.github.com>
2024-11-08 09:20:38 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   rootElement
 );
